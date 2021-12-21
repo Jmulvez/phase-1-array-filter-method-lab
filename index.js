7b1be88ed521c9413e24dfe083be504e64998de6
John <johnmulvey48@yahoo.com>
2021-12-21 13:09:27 -0500

    git@github.com:Jmulvez/phase-1-array-filter-method-lab.git

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(name) {
+    for (const drivers of name) {
+     console.log(drivers.name);   
+    }
+}
+
+function fuzzyMatch(name) {
+    for (const drivers of name) {
+        if (name === "Sa") {
+            console.log(name);
+        }
+    }
+}
+
+function matchName(drivers) {
+    for (const name of drivers) {
+        console.log(name, hometown);
+    }
+}
